1f1a53dad75ea32a7f533b0dd9cbe8f32e599855
Travis Jefferson <traviscjefferson@gmail.com>
2020-04-26 17:54:47 -0400

    ns-qualified keywords for db/tx-data

--- src/kitchen/server.clj
+++ src/kitchen/server.clj
@@ -11,14 +11,14 @@
   {:name :database-interceptor
    :enter (fn [context]
             ;; context.request.database = @database
-            (update context :request assoc :database @database))
+            (update context :request assoc ::database @database))
    :leave (fn [context]
-            (if-let [[op & args] (:tx-data context)]
+            (if-let [[op & args] (::tx-data context)]
               (do
                 ;; if someone has attached tx-data, "run the transaction" here
                 ;; and attach the new db value
                 (apply swap! database op args)
-                (assoc-in context [:request :database] @database))
+                (assoc-in context [:request ::database] @database))
               context))})
 
 (def supported-types ["text/html" "application/edn" "application/json" "text/plain"])
@@ -39,7 +39,7 @@
             (let [command-params (get-in context [:request :query-params :command-params])
                   new-command (commander/create-command command-params)]
               (assoc context
-                     :tx-data [assoc (:id new-command) new-command]
+                     ::tx-data [assoc (:id new-command) new-command]
                      :response (ok "YAY"))))})
 
 
