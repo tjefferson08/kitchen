7f40d636b3b783f43c53557d0ba0115d42785d0e
Travis Jefferson <traviscjefferson@gmail.com>
2020-04-26 23:32:54 -0400

    Macro for starting/stopping test systems

--- test/kitchen/system-test.clj
+++ test/kitchen/system-test.clj
@@ -10,11 +10,17 @@
 (defn service-fn [system]
   (get-in system [:server :service ::http/service-fn]))
 
+(defmacro with-system
+  [[bound-var binding-expr] & body]
+  `(let [~bound-var (component/start ~binding-expr)]
+     (try
+       ~@body
+       (finally
+         (component/stop ~bound-var)))))
+
 (deftest test-create-command
-  (let [test-sys (build-system)
-        started-sys (component/start test-sys)]
+  (with-system [test-sys (build-system)]
     (is (= 200
            (:status (response-for
-                     (service-fn started-sys)
-                     :post "/commands"))))
-    (component/stop test-sys)))
+                     (service-fn test-sys)
+                     :post "/commands"))))))
